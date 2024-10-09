99f8f5117df6f7ab6e3daf77b38a16030e5f1117
Ana Rad <cruelhamster25@gmail.com>
2024-10-09 06:39:30 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,42 @@
+class Horse:
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)  # Инициализация родительского класса Horse
+        Eagle.__init__(self)  # Инициализация родительского класса Eagle
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+# Создание объекта класса Pegasus
+p1 = Pegasus()
+
+# Проверка работы методов
+print(p1.get_pos())  # (0, 0)
+p1.move(10, 15)
+print(p1.get_pos())  # (10, 15)
+p1.move(-5, 20)
+print(p1.get_pos())  # (5, 35)
+
+p1.voice()  # I train, eat, sleep, and repeat
